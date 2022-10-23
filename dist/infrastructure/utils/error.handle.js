94e69db26853d12f487f5f09bb6b92f6b4e93e7c
daniel.benavente <daniel.benavente4@gmail.com>
2022-10-23 12:57:59 +0200

    remove dist

--- dist/infrastructure/utils/error.handle.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleHttp = void 0;
-const handleHttp = (res, error) => {
-    res.status(500);
-    res.send({ error });
-};
-exports.handleHttp = handleHttp;
